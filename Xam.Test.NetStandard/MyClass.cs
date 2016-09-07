4bb4175ec669b5c3086b5a40fcbe381e7d08b459
Matt Ward <matt.ward@xamarin.com>
2016-09-07 14:07:29 +0100

    Add project with NuGet package

---
+++ Xam.Test.NetStandard/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xam.Test.NetStandard
+{
+    public class MyClass
+    {
+        public string GetMessage ()
+        {
+            return "Hello .NET Standard";
+        }
+    }
+}
